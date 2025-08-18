fbf10b274275662134629b4e48288ee31727eb1c
undevy <undevy@gmail.com>
2025-08-18 09:50:17 +0400

    feat(ui): FIX: Updated ProfileBoot Screen

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,3 +1,4 @@
+// File: /Users/undevy/portfolio-project/src/app/page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
