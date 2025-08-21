422cab48982d74e134d8456cb7866a722d7cf9ea
undevy <undevy@gmail.com>
2025-08-21 15:21:07 +0400

    CRITICAL: Remove .next from repository and fix .gitignore
    BREAKING CHANGE: Removes all .next files from version control
    
    Problem:
    - The .next folder was incorrectly tracked in Git
    - Only .next/cache/ was in .gitignore instead of entire .next/
    - This caused repository bloat and merge conflicts
    
    Solution:
    - Fixed .gitignore to ignore entire .next/ directory
    - Removed all .next files from repository (git rm --cached)
    - Files remain on disk for local development
    
    Impact:
    - Repository size will be significantly reduced
    - No more merge conflicts from build artifacts
    - Cleaner git history going forward
    
    Note: After pulling this change, other developers should:
    1. Run 'git pull'
    2. Run 'npm run build' to regenerate their local .next

--- .next/server/chunks/7948.js
+++
@@ -1,8 +0,0 @@
-"use strict";exports.id=7948,exports.ids=[7948],exports.modules={77948:(a,b,c)=>{c.r(b),c.d(b,{chevronLeftSvg:()=>e});var d=c(50861);let e=(0,d.JW)`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`}};
